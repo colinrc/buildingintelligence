0c54a6d66bb0ba096e97fa94a926643643907b32
dave <dave@bi>
2006-01-26 02:45:32 +0000

    Zone partition request for M1

---
+++ server/src/au/com/BI/M1/Commands/ZonePartitionRequest.java
@@ -0,0 +1,15 @@
+package au.com.BI.M1.Commands;
+
+public class ZonePartitionRequest extends M1Command {
+
+	public ZonePartitionRequest() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public ZonePartitionRequest(String sum, String use) {
+		super(sum, use);
+		// TODO Auto-generated constructor stub
+	}
+
+}
