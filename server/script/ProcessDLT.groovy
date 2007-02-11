2320b7e285911826efc23ffb7d892d638696d7b3
colin <colin@bi>
2007-02-11 11:01:15 +0000

    Changed for 1.6b37 script updates

--- server/script/ProcessDLT.groovy
+++ server/script/ProcessDLT.groovy
@@ -1,37 +1,32 @@
 import au.com.BI.Script.*
 import au.com.BI.Command.*
 
- public class ProcessDLT  implements BIScript  {
- 
-   // true if the script can be run multiple times in parallel
-    boolean ableToRunMultiple = false
-    
-    // List the variables that will cause the script to run when they change
-    String[]  fireOnChange =   ["DLT_1", "DLT_2","DLT_3","DLT_4"]
-    
-    // If the script is able to be stopped before completion, generally no
-    boolean  stoppable = false;
- 
-    
-   // Include these lines as they are in every script
-	def elife  = "${elife}"
-	def triggeringEvent = "${triggeringEvent}"
-	def cache = "${cache}"
-	def labelMgr = "{labelMgr}"
+
+public class ProcessDLT  extends BIScript  {
 	
+   // true if the script can be run multiple times in parallel
+boolean ableToRunMultiple = false
+
+// List the variables that will cause the script to run when they change
+String[]  fireOnChange =   ["DLT_1", "DLT_2","DLT_3","DLT_4"]
+
+// If the script is able to be stopped before completion, generally not
+boolean  stoppable = false;
+
+
 	def main (String[] argv) {
 	// The action contents of the script go here
-	
-		if (triggeringEvent  == null) {
+		
+		if (triggeringEvent  == null || triggerWasScript ) {
 			return
 			// Just in case the script was run from the Flash control plan, rather than picking up a message change.
 		}
-		
+			
 		// to read something from the label manager in groovy
-		def currentLabel = labelMgr.getLabelState (triggeringEvent.getDisplayName ())
+		def currentLabel = labelMgr.getLabelState (triggerDisplayName)
 		elife.log ("Label " + currentLabel)
 		
-		switch (triggeringEvent.getDisplayName ()) {
+		switch (triggerDisplayName) {
 		case "DLT_1": 
 			if (currentLabel == "ON") {
 				elife.sendCommand ("KITCHENAV","on","100")
@@ -60,27 +55,27 @@ import au.com.BI.Command.*
 				elife.sendCommand("KITCHEN_AV","src","cd2")
 				elife.sendCommand("DLT_2","label","DVD")
 				break
-				
+			
 			case "DVD":
 				elife.sendCommand( "KITCHEN_AV","KITCHEN_AV","src","dvd")
 				elife.sendCommand("DLT_2","label","TV")
 				break
-
+		
 			case "TV":
 				elife.sendCommand( "KITCHEN_AV","KITCHEN_AV","src","tv")
 				elife.sendCommand("DLT_2","label","CD1")
 				break
-
+		
 			}
 			break
 			
 		case "DLT_3" :
 			// third button send volume
-			elife.sendCommand( "KITCHEN_AV","volume",triggeringEvent.getExtraInfo())
+			elife.sendCommand( "KITCHEN_AV","volume",triggerExtra)
 			break
-
+		
 			
 		}
-				   
 	}
 }
+
