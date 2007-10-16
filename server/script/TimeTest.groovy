91666c70af03d651b73992a78c2f732bfc6fc12a
merik <merik@bi>
2007-10-16 22:14:58 +0000

    Attempts to use time retriggered delay functions using groovy and a macro

--- server/script/TimeTest.groovy
+++ server/script/TimeTest.groovy
@@ -1,5 +1,9 @@
 import au.com.BI.Script.*
 import au.com.BI.Command.*
+import au.com.BI.Util.*
+import java.util.logging.Level
+import groovy.time.*
+import java.util.*
 
 
 public class TimeTest  extends BIScript  {
@@ -8,7 +12,7 @@ public class TimeTest  extends BIScript  {
 boolean ableToRunMultiple = false
 
 // List the variables that will cause the script to run when they change
-String[]  fireOnChange =   ["DLT_1", "DLT_2","DLT_3","DLT_4"]
+String[]  fireOnChange =   ["TIME_TEST","TIMETEST_TIMER"]
 
 // If the script is able to be stopped before completion, generally not
 boolean  stoppable = false;
@@ -16,31 +20,46 @@ boolean  stoppable = false;
 
 	def main (String[] argv) {
 	// The action contents of the script go here
-		
-		if (triggeringEvent  == null || triggerWasScript ) {
-			return
-			// Just in case the script was run from the Flash control plan, rather than picking up a message change.
-		}
-			
-		// to read something from the label manager in groovy
-		def currentLabel = labelMgr.getLabelState (triggerDisplayName)
-		elife.log ("Label " + currentLabel)
-		
-		switch (triggerDisplayName) {
-		case "DLT_1": 
-			if (currentLabel == "ON") {
-				elife.sendCommand ("KITCHENAV","on","100")
-				 elife.sendCommand("DLT_1","label","OFF")
-			}
-			if (currentLabel == "OFF") {
-				elife.sendCommand ("KITCHENAV","off","0")
-				 elife.sendCommand("DLT_1","label","ON")
-				 elife.sendCommand ("KITCHENAV","off","0")
-				 elife.sendCommand("DLT_1","label","ON")
-			}
-		break
-			
-		}
+	
+	if (triggerWasScript ) {
+		return
+		// Just in case the script was run from the Flash control plan, rather than picking up a message change.
 	}
+	
+	switch (triggerDisplayName) {
+	case "TIMETEST_TIMER":
+		switch (triggerCommand) {
+		case "on":
+		break
+
+		case "off":
+			elife.sendCommand ("HALL_LIGHTS","off")
+		break
+		}
+		
+	case "TIME_TEST":
+		cal = Calendar.getInstance()
+		hour = cal.get(Calendar.HOUR_OF_DAY)
+		elife.log ("TIME IS " + hour)
+		switch (hour) {
+		case 0: case 1: case 2: case 3: case 4: case 5: case 6:
+		case 7: case 21: case 22: case 23: case 24:
+			elife.log ("NIGHT TIME")
+			elife.sendCommand ("HALL_LIGHTS","on","30")
+			elife.sendCommand ("MACRO","abort","TimeTestTimer")
+			elife.sendCommand ("MACRO","run","TimeTestTimer")
+			break
+		case 18: case 19: case 20:
+			elife.log ("EVENING TIME")
+			elife.sendCommand ("HALL_LIGHTS","on","100")
+			elife.sendCommand ("MACRO","abort","TimeTestTimer")
+			elife.sendCommand ("MACRO","run","TimeTestTimer")
+			break
+		default :
+			elife.log ("DAY TIME")
+		}
+	break
+	}	
 }
 
+}
