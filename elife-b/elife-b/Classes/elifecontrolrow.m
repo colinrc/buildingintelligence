70860c3bd0f2cf0b15db4bbdec740f34f79e3e58
cameron <cameron@bi>
2009-03-15 09:57:09 +0000

    Extra level of abstraction to cover conditional rows in controls

---
+++ elife-b/elife-b/Classes/elifecontrolrow.m
@@ -0,0 +1,16 @@
+//
+//  elifecontrolrow.m
+//  elife-b
+//
+//  Created by Cameron Humphries on 19/01/09.
+//  Copyright 2009 Humphries Consulting Pty Ltd. All rights reserved.
+//
+
+#import "elifecontrolrow.h"
+
+
+@implementation elifecontrolrow
+@synthesize rowattrs;
+@synthesize displayitems;
+
+@end
