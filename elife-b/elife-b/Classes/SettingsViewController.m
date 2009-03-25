c9f552d941184181b108f9deff7d00444dc84865
cameron <cameron@bi>
2009-03-25 20:07:03 +0000

    Added IB elements for Settings Tab

--- elife-b/elife-b/Classes/SettingsViewController.m
+++ elife-b/elife-b/Classes/SettingsViewController.m
@@ -10,16 +10,19 @@
 
 
 @implementation SettingsViewController
+@synthesize localserverip;
+@synthesize localserverport;
+@synthesize localconfigname;
 
-/*
 // Override initWithNibName:bundle: to load the view using a nib file then perform additional customization that is not appropriate for viewDidLoad.
 - (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil {
     if (self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil]) {
         // Custom initialization
+		
     }
     return self;
 }
-*/
+
 
 /*
 // Implement loadView to create a view hierarchy programmatically.
@@ -27,12 +30,21 @@
 }
 */
 
-/*
+
 // Implement viewDidLoad to do additional setup after loading the view.
 - (void)viewDidLoad {
     [super viewDidLoad];
+	// Grab values from system prefs
+	NSUserDefaults *userDefaults = [NSUserDefaults standardUserDefaults];  
+	NSString *elifehost = [userDefaults stringForKey:@"local_server_ip"];  
+	NSString *elifeport = [userDefaults stringForKey:@"local_server_port"];
+	NSString *defconfig = [userDefaults stringForKey:@"config_file"];	
+	[self.localserverip setText:elifehost];
+	[self.localserverport setText:elifeport];
+	[self.localconfigname setText:defconfig];
+	
 }
-*/
+
 
 
 - (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation {
@@ -51,5 +63,22 @@
     [super dealloc];
 }
 
+- (IBAction)finishEdit:(id)sender {
+	UITextField *mytextField = (UITextField *)sender;
+	// close keyboard
+	[mytextField resignFirstResponder];
+	
+	// save userprefs
+	NSUserDefaults *userDefaults = [NSUserDefaults standardUserDefaults];  
+	if (mytextField.tag == 2) {
+		[userDefaults setObject:mytextField.text forKey:@"local_server_ip"];
+	} else if (mytextField.tag == 3) {
+		[userDefaults setObject:mytextField.text forKey:@"local_server_port"];
+	} else if (mytextField == 4) {
+		[userDefaults setObject:mytextField.text forKey:@"config_file"];
+	}
+	
+	
+}
 
 @end
