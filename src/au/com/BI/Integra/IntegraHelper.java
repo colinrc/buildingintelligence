b7a6036e339b381739f3450381b79270a83fc3ae
richard <richard@bi>
2010-04-14 23:00:26 +0000

    first checkin of Integra code: tested ip comms with nc commands volume, mute, select line in, power and preset have been inplemented

---
+++ src/au/com/BI/Integra/IntegraHelper.java
@@ -0,0 +1,383 @@
+/**
+ * 
+ */
+package au.com.BI.Integra;
+
+import au.com.BI.Util.Utility;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import org.apache.log4j.lf5.LogLevel;
+
+/**
+ * @author richardlemon
+ * Helper class to encapsulate the Integra protocol specific code.
+ * Use to create and interpret the data from the Integra
+ */
+public class IntegraHelper {
+
+	protected String deviceModel = null;	// the model number of the hardware
+	protected String commsLine = null;		// the type of comms, serial or ip
+	protected final String STX = "!";		// start of TX
+	protected final String ETX = "\n";		// end of TX
+	protected final String ERX = "\0x1A";	// end of RX
+	protected final String UnitType = "1";	// the unit type always 1 for receiver
+	protected Logger logger = null;
+	
+	/**
+	 * Enumeration  allows us to map commands to and from device
+	 * @author richardlemon
+	 */
+	public enum commandMap 
+	{
+		// enum elements
+		power ("PWR","ZPW","PW3"), 
+		mute("AMT", "ZMT", "MT3"),
+		volume("MVL", "ZVL", "VL3"),
+		src ("SLI", "SLZ", "SLZ"),
+		preset("LMD","LMZ",null);
+		// member variables
+		private final String zone1;
+		private final String zone2;
+		private final String zone3;
+		// constructor
+		commandMap(String zone1, String zone2, String zone3)
+		{
+			this.zone1 = zone1;
+			this.zone2 = zone2;
+			this.zone3 = zone3;
+		}
+		/**
+		 * Get the flash command for the given Integra command
+		 * @param cmd	The Integra command received on the serial port
+		 * @return	string value of the flash command, null if not found
+		 */
+		public String getFlashString(String cmd)
+		{
+			if (zone1.equals(cmd) || zone2.equals(cmd) || zone3.equals(cmd)){
+				return  toString();
+			}
+			return null;
+		}
+		/**
+		 * Get the Integra command for the given flash command
+		 * @param cmd	The Flash command received from the client
+		 * @param zone	The zone that we are working on
+		 * @return 		value of the Integra command, null if not found
+		 */
+		public String getIntegraCommand(int zone){
+			switch(zone)
+			{
+			case 1:
+				return zone1;
+			case 2:
+				return zone2;
+			case 3:
+				return zone3;
+			}
+			return null;
+		}
+	};
+	/**
+	 * Standard constructor
+	 * @param modelName - the model name of the device we are controlling
+	 * @param lineOut - string either IP or SERIAL
+	 */
+	public IntegraHelper(Logger logger, String modelName, String lineOut) {
+		this.logger = logger;
+		deviceModel = modelName;
+		commsLine = lineOut;
+	}
+	/**
+	 * tests the comm line type
+	 * @return true if the comms are serial
+	 */
+	public boolean isSerial(){
+		// TODO decide if we would be better off optimizing the string comms test to bool or int 
+		return commsLine.equals("SERIAL");
+	}
+	/**
+	 * Tests the comm line type
+	 * @return true if the comms are tcp/ip
+	 */
+	public boolean isIP(){
+		return commsLine.equals("IP");
+	}
+	/**
+	 * gets the start of transmission character
+	 * @return either STX for serial or null for TCP/IP
+	 */
+	public String getSTX(){
+		String retStr = null;
+		if (isSerial())
+			retStr = STX;
+		return retStr;
+	}
+	/**
+	 * gets the end of transmission character
+	 * @return either STX for serial or null for TCP/IP
+	 */
+	public String getETX(){
+		return ETX;
+	}
+	/**
+	 * Get the source device from the received serial string
+	 * @param inCommand	The received serial string
+	 * @return boolean the true if source == UnitType constant
+	 */
+	public boolean validSourceDevice(String inCommand){
+		try
+		{
+			if (isSerial())
+				return inCommand.substring(1, 2).equals(UnitType );
+			
+			return inCommand.substring(0,1).equals(UnitType);
+		}
+		catch(IndexOutOfBoundsException ex)
+		{
+			logger.log (Level.WARNING, "Received command to short for Integra, cant get source device");
+		}
+		return false;
+	}
+	/**
+	 * Get the input parameter for a command
+	 * @param inCommand the received serial string from the device
+	 * @return	string the action part of command (on / off / qstn) etc, empty on fail
+	 */
+	public String getInputValue(String inCommand){
+		try
+		{
+		if (isSerial())
+			return inCommand.substring(5);
+		
+		return inCommand.substring(4);
+		}
+		catch(IndexOutOfBoundsException ex)
+		{
+			logger.log (Level.WARNING, "Received command to short for Integra, couldn't read command data");			
+		}
+		return "";
+	}
+	/**
+	 * Get the 3 character command code 
+	 * @param inCommand	the received serial string from the device
+	 * @return	string the command code, empty on fail
+	 */
+	public String getInputCommand(String inCommand){
+		try
+		{
+			if (isSerial())
+				return inCommand.substring(2,5);
+			
+			return inCommand.substring(1,4);
+		}
+		catch(IndexOutOfBoundsException ex)
+		{
+			logger.log (Level.WARNING, "Received command to short for Integra, couldn't read command code");
+		}
+		return "";
+	}
+	/**
+	 * Using the format of the command figure out which device we are looking at
+	 * If the last character of the command is a 3 (zone 3)
+	 * if the first or last is a Z (Zone 2)
+	 * else default (Zone 1)
+	 * @param command	Input command string
+	 * @return	Device of interest
+	 */
+	String getDeviceKey(String command){
+		try
+		{
+			String key = new String("01");
+			String cmd = getInputCommand(command);
+			if (cmd.charAt(0) == 'Z' || cmd.charAt(2) == 'Z')
+				key = "02";
+			if (cmd.charAt(2) == '3')
+				key = "03";
+			
+			return key;
+		}
+		catch(IndexOutOfBoundsException ex)
+		{
+			logger.log (Level.WARNING, "Received command to short for Integra, cant get device key");			
+		}
+		return "";
+	}
+	/**
+	 * Given the hex volume value scale to 0..100
+	 * @param volume hex volume string from device
+	 * @return scaled volume value 0..100
+	 */
+	int scaleVolume(String volume)
+	{
+		int retVal = -1;
+		try
+		{
+			int nVal = Integer.parseInt(volume, 16);
+			// if type == dtr5.2 or dtr6.2 then 0..80 is full scale
+			// else 0..100 is full scale
+			if (deviceModel == "DTR5.2" || deviceModel == "DTR6.2"){
+				// scale 0..80 to 0..100
+				 retVal = Utility.scaleForFlash(String.valueOf(nVal), 0, 80,false);
+			}
+			else 
+				retVal = Utility.scaleForFlash(String.valueOf(nVal), 0, 100,false);
+			}
+		catch(NumberFormatException ex)
+		{
+			logger.log (Level.WARNING,"Volume value invalid " + ex.getMessage());
+		}
+		if ( retVal > 100) retVal = -1; 
+		return retVal;
+	}
+	/**
+	 * creates output volume string to send to the device
+	 * @param zone	integer value for zone [1,2,3]
+	 * @param action	either volume up or down	
+	 * @return
+	 */
+	public String outVolume(int zone, String action){
+		String outCommand = new String();
+		// TODO test is probably unnecessary string + null = string?
+		if (isSerial())
+			outCommand += getSTX();
+		outCommand += UnitType;
+
+		String integraCmd = commandMap.volume.getIntegraCommand(zone);
+		if (integraCmd == null)
+			return null;
+		outCommand += integraCmd;
+		if (action.equals("up"))
+		{
+			outCommand += "UP" + getETX();
+		}
+		else if (action.equals("down"))
+		{
+			outCommand += "DOWN" + getETX();
+		}
+		else if (action.equals("query"))
+		{
+			outCommand += "QSTN" + getETX();
+		}
+		else 
+			outCommand = null;
+		
+		return outCommand;
+	}
+	/**
+	 * Creates an output mute string to send to the device
+	 * @param zone	integer value for zone [1,2,3]
+	 * @param action	either mute on or off	
+	 * @return
+	 */
+	public String outMute(int zone, String action)
+	{
+		String outCommand = new String();
+		// TODO test is probably unnecessary string + null = string?
+		if (isSerial())
+			outCommand += getSTX();
+		outCommand += UnitType;
+
+		String integraCmd = commandMap.mute.getIntegraCommand(zone);
+		if (integraCmd == null)
+			return null;
+		
+		outCommand += integraCmd;
+		if (action.equals("on"))
+		{
+			outCommand += "01" + getETX();
+		}
+		else if (action.equals("off"))
+		{
+			outCommand += "00" + getETX();
+		}
+		else if (action.equals("query"))
+		{
+			outCommand += "QSTN" + getETX();
+		}
+		else 
+			outCommand = null;
+		
+		return outCommand;
+		
+	}
+	/**
+	 * Creates an output mute string to send to the device
+	 * @param zone	integer value for zone [1,2,3]
+	 * @param action	either mute on or off	
+	 * @return
+	 */
+	public String outPower(int zone, String action)
+	{
+		String outCommand = new String();
+		// TODO test is probably unnecessary string + null = string?
+		if (isSerial())
+			outCommand += getSTX();
+		outCommand += UnitType;
+
+		String integraCmd = commandMap.power.getIntegraCommand(zone);
+		if (integraCmd == null)
+			return null;
+		
+		outCommand += integraCmd;
+		if (action.equals("on"))
+		{
+			outCommand += "01" + getETX();
+		}
+		else if (action.equals("off") || action.equals("standby"))
+		{
+			outCommand += "00" + getETX();
+		}
+		else if (action.equals("query"))
+		{
+			outCommand += "QSTN" + getETX();
+		}
+		else 
+			outCommand = null;
+		
+		return outCommand;
+		
+	}
+	/**
+	 * Creates an output select source string to send to the device
+	 * @param zone	integer value for zone [1,2,3]
+	 * @param action the code for the source to select (see config_integra.xml)	
+	 * @return
+	 */
+	public String outSource(int zone, String action)
+	{
+		String outCommand = new String();
+		// TODO test is probably unnecessary string + null = string?
+		if (isSerial())
+			outCommand += getSTX();
+		outCommand += UnitType;
+
+		String integraCmd = commandMap.src.getIntegraCommand(zone);
+		if (integraCmd == null)
+			return null;
+		
+		outCommand += integraCmd + action + getETX();
+		return outCommand;
+	}
+	/**
+	 * Creates an output select listening mode string
+	 * @param zone	integer value for zone [1,2,3]
+	 * @param action the code for the listening mode to select (see config_integra.xml)
+	 * @return
+	 */
+	public String outPreset(int zone, String action)
+	{
+		String outCommand = new String();
+		// TODO test is probably unnecessary string + null = string?
+		if (isSerial())
+			outCommand += getSTX();
+		outCommand += UnitType;
+
+		String integraCmd = commandMap.preset.getIntegraCommand(zone);
+		if (integraCmd == null)
+			return null;
+		
+		outCommand += integraCmd + action + getETX();
+		return outCommand;
+	}
+}
---
+++ src/au/com/BI/Integra/Model.java
@@ -0,0 +1,389 @@
+package au.com.BI.Integra;
+
+import java.util.Map;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import au.com.BI.Command.CommandInterface;
+import au.com.BI.Command.ReturnWrapper;
+import au.com.BI.Comms.CommsFail;
+import au.com.BI.Config.ParameterException;
+import au.com.BI.Device.DeviceType;
+import au.com.BI.Util.SimplifiedModel;
+import au.com.BI.Util.DeviceModel;
+
+public class Model extends SimplifiedModel implements DeviceModel {
+
+	//protected HashMap <String,AVState>state;
+	protected Logger logger = null;
+	protected IntegraHelper integraHelper = null;
+	protected String deviceModel = null;	// the model number of the hardware
+	
+	/**
+	 * Std constructor
+	 * Initializes 
+	 */
+	public Model () {
+		super();
+		logger = Logger.getLogger(this.getClass().getPackage().getName());
+		//state = new HashMap<String,AVState>();
+	}
+	/**
+	 * Clears the state map and the config
+	 */
+	public void clearItems () {
+		//state.clear();
+		super.clearItems();
+	}
+	/**
+	 * Add all of the state objects 
+	 
+	public void prepareToAttatchComms() {
+		for (DeviceType avDevice : configHelper.getAllOutputDeviceObjects()){
+			String key  = avDevice.getKey();
+			state.put(key, new AVState());
+		}
+	}
+	*/
+	/**
+	 * Get the Integra model string
+	 */
+	public void finishedReadingParameters () {
+		super.finishedReadingParameters();
+
+		deviceModel = getParameterValue("MODEL", DeviceModel.MAIN_DEVICE_GROUP);
+		String line = this.getParameterValue("Connection_Type",DeviceModel.MAIN_DEVICE_GROUP);
+		integraHelper = new IntegraHelper(logger, deviceModel,line);
+		if (line.equals("IP"))
+			setInterCommandInterval(500);
+		else
+			setInterCommandInterval(75);			
+	}
+	/**
+	 * Startup method called to get the class ready
+	 * clears the comms and deals with the XML config
+	 */
+	public void doStartup() throws CommsFail {
+		
+		synchronized (comms) {
+			comms.clearCommandQueue();
+		}
+		
+		for (DeviceType avDevice : configHelper.getAllOutputDeviceObjects()){
+			String strCmd = null;
+			// loop through the device config initializing the devices
+			strCmd = buildOutputString(avDevice, "mute", "query");
+			if (strCmd != null)
+				comms.sendString(strCmd);
+			strCmd = buildOutputString(avDevice, "volume", "query");
+			if (strCmd != null)
+				comms.sendString(strCmd);
+			strCmd = buildOutputString(avDevice, "power", "query");
+			if (strCmd != null)
+				comms.sendString(strCmd);
+			strCmd = buildOutputString(avDevice, "src", "query");
+			if (strCmd != null)
+				comms.sendString(strCmd);
+			// FIXME may need to change preset to something else...
+			strCmd = buildOutputString(avDevice, "preset", "query");
+			if (strCmd != null)
+				comms.sendString(strCmd);
+		}
+	}
+	/**
+	 * Sends a command on the serial port to the Integra device
+	 * @param command 	The command from flash or other client
+	 * @throws CommsFail throws from the comms classes if something goes very wrong 
+	 */
+	public void doOutputItem (CommandInterface command) throws CommsFail {
+		String theWholeKey = command.getKey();
+		DeviceType device  = configHelper.getOutputItem(theWholeKey);
+		String toSend = null;
+		
+		if (device == null) {
+			logger.log(Level.SEVERE, "Error in config, no output key for " + theWholeKey);
+		}
+		else {
+			cache.setCachedCommand(command.getKey(),command);
+
+			String theCommand = command.getCommandCode();
+			String extra = ((String)command.getExtraInfo());
+
+			toSend = buildOutputString(device, theCommand, extra);
+			if (toSend != null && !toSend.isEmpty()) {
+
+				logger.log(Level.FINER, "Integra event for zone " + device.getKey() + " received from flash");
+				comms.sendString(toSend);
+				logger.log (Level.FINEST,"Sent integra command " + toSend);
+			}
+			else {
+				logger.log (Level.WARNING,"Error processing Integra message, no message to send");
+			}
+		}
+	}
+	/**
+	 * Given a command get the device it corresponds to
+	 * @param command	Input command string
+	 * @return	Device of interest
+	 */
+	DeviceType getDevice(String command){
+		String key = integraHelper.getDeviceKey(command);
+		if (key.equals(""))
+			return null;
+		return configHelper.getControlItem(formatKey(key, null));
+	}
+	/**
+	 * 
+	 * @param srcCode
+	 * @param catalogName
+	 * @param device
+	 * @return
+	 * @throws ParameterException
+	 */
+	public String getKeyForParameterValue(String srcCode, String catalogName,
+			DeviceType device) throws ParameterException {
+
+		String groupName = device.getGroupName();
+		String paramMapName = getParameterValue(catalogName, groupName);
+		Map<String, String> inputParameters = getCatalogueDef(paramMapName);
+		String returnVal = "";
+
+		for (String eachItem : inputParameters.keySet()) {
+			if (inputParameters.get(eachItem).equals(srcCode)) {
+				returnVal = eachItem;
+				break;
+			}
+		}
+		if (returnVal.equals(""))
+			throw new ParameterException(
+					"An input device has been selected which has not been configured, please contact your integrator");
+		return returnVal;
+	}
+
+	/**
+	 * Controlled item is the default item type. 
+	 * The system will call this function if it is not from flash.
+	 * ie. It is from the serial port.
+	 * @param command	The command received on the serial port
+	 */
+	public void doControlledItem (CommandInterface command) throws CommsFail
+	{
+		DeviceType avDevice = getDevice(command.getKey());
+		ReturnWrapper commandObject = interpretStringFromIntegra (command, avDevice);
+		for (CommandInterface eachCommand: commandObject.getOutputFlash()){
+			this.sendToFlash(eachCommand, cache);
+		}
+	}
+	/**
+	 * Get the state instance from the state map for the key provided
+	 * @param key	The key for the device we are interested in
+	 * @return	The correct state variable
+	 * TODO decide whether we need to keep the state
+	public AVState getState(String key) {
+		AVState currentState = state.get(key);
+		if (currentState == null){
+			currentState = new AVState();
+			state.put(key, currentState);
+		}
+		return currentState;
+	}
+	 */
+	/**
+	 * Protocol handling function
+	 * @param command 	The command received on the serial port
+	 * @return ReturnWrapper	The encapsulated return type
+	 * TODO This may not work as the serial string is terminated with EOF (^Z)
+	 */
+	public ReturnWrapper interpretStringFromIntegra (CommandInterface command, DeviceType avDevice){
+		ReturnWrapper result = new ReturnWrapper();
+
+		String recvdCmd = command.getKey();
+		// lets pull apart the string a little
+		String val = integraHelper.getInputValue(recvdCmd);
+		String cmd = integraHelper.getInputCommand(recvdCmd);
+		
+		// get the state
+		//AVState currentState = getState (avDevice.getKey());
+		
+		// if it is not am amplifier we don't know what to do with it
+		if (!integraHelper.validSourceDevice(recvdCmd))
+		{
+			logger.log (Level.WARNING, "Received command not from Integra amplifier " + recvdCmd);
+			//logger.log (Level.WARNING, "Expected 1 got " + String.valueOf(srcType));
+			result.setError(true);
+			result.setErrorDescription( "Received command not from Integra amplifier " + recvdCmd);
+			return result;
+		}
+		
+		// command switch..
+		/* TODO change to switch on the flash commands, need to change the getCommand()?
+		 * TODO change to a switch? think you can switch on strings in java...
+		*/
+		if (cmd.equals("PWR") || cmd.equals("ZPW") || cmd.equals("PW3"))
+		{
+			logger.log (Level.FINE, "Received power command " + cmd);
+			// if command is off and currently on
+			if (val.equals("00")){ // && currentState.testPower(true)){
+				// power off (standby)
+				result.addFlashCommand(buildCommandForFlash (avDevice, "off","","","","","",0));
+				//currentState.setPower (false);
+			} // if command is on and currently off
+			else if (val.equals("01")){ // && currentState.testPower(false)){
+				// power on 
+				result.addFlashCommand(buildCommandForFlash (avDevice, "on","","","","","",0));
+				//currentState.setPower (true);
+			}
+			else
+			{
+				result.setError(true);
+				result.setErrorDescription("Power command returned unknown value ["+ val +"]");
+			}
+		}
+		else if (cmd.equals("AMT") || cmd.equals("ZMT") || cmd.equals("MT3"))
+		{
+			logger.log (Level.FINE, "Received mute command " + cmd);
+			if (val.equals("00")){ //&& currentState.isMute()){
+				result.addFlashCommand(buildCommandForFlash (avDevice, "mute","off","","","","",0));
+				//currentState.setMute(false);				
+			}
+			else if (val.equals("01")){ //&& !currentState.isMute()){
+				result.addFlashCommand(buildCommandForFlash (avDevice, "mute","on","","","","",0));
+				//currentState.setMute(true);				
+			}
+			else
+			{
+				result.setError(true);
+				result.setErrorDescription("Mute command returned unknown value ["+ val +"]");
+			}
+		}
+		else if (cmd.equals("MVL") || cmd.equals("ZVL") || cmd.equals("VL3"))
+		{
+			logger.log (Level.FINE, "Received volume command " + cmd);
+			int volForFlash = integraHelper.scaleVolume(val);
+			
+			if (volForFlash > 0)
+			{
+				String volForFlashString = String.valueOf(volForFlash);
+				//currentState.setVolume(volForFlashString);
+				result.addFlashCommand(buildCommandForFlash ( avDevice, "volume",volForFlashString,"","","","",0));					
+			}
+		}
+		else if (cmd.equals("SLI") || cmd.equals("SLZ") || cmd.equals("SL3"))
+		{
+			logger.log (Level.FINE, "Received line in command " + cmd);
+
+			try {
+				// get the code to send to flash
+				String newSrc = getKeyForParameterValue(val, "AV_INPUTS", avDevice);
+				// listening mode command
+				result.addFlashCommand(buildCommandForFlash(avDevice, "src",newSrc,"","","","",0));
+			} catch (ParameterException ex) {
+				result.setError(true);
+				result.setErrorDescription(ex.getMessage());
+			}
+		}
+		else if (cmd.equals("LMD"))
+		{
+			logger.log (Level.FINE, "Received listening mode command " + cmd);
+			try
+			{
+				// get the code to send to flash
+				String newSrc = getKeyForParameterValue(val, "AV_SURROUND", avDevice);
+				// listening mode command
+				result.addFlashCommand(buildCommandForFlash(avDevice, "preset",newSrc,"","","","",0));
+			}
+			catch (ParameterException ex) {
+				result.setError(true);
+				result.setErrorDescription(ex.getMessage());
+			}
+		}
+		else if (cmd.equals("LMZ"))
+		{
+			logger.log (Level.FINE, "Received listening mode command " + cmd);
+			try
+			{
+				// get the code to send to flash
+				String newSrc = getKeyForParameterValue(val, "AV_SURROUND_ZONE2", avDevice);
+				// listening mode command
+				result.addFlashCommand(buildCommandForFlash(avDevice, "preset",newSrc,"","","","",0));
+			}
+			catch (ParameterException ex) {
+				result.setError(true);
+				result.setErrorDescription(ex.getMessage());
+			}
+		}
+		else if (cmd.equals(""))
+		{
+			// we don't handle this command just yet
+			logger.log (Level.WARNING,"Malformed command from Integra " + recvdCmd);
+			result.setError(true);
+			result.setErrorDescription("Malformed command from Integra " + recvdCmd);			
+		}
+		else
+		{
+			// we don't handle this command just yet
+			logger.log (Level.WARNING,"We do not handle command ["+ cmd +"] from Integra yet");
+			result.setError(true);
+			result.setErrorDescription("We do not handle command ["+ cmd +"] from Integra yet");
+		}
+		
+		return result;
+	}
+	/**
+	 * builds the output string for the integra device for the command 
+	 * received from flash client
+	 * @param device
+	 * @param command
+	 * @return ReturnWrapper	the encapsulated return command
+	 */
+	public String buildOutputString (DeviceType device, String theCommand, String extra){
+		String returnVal = null;
+		int key = Integer.parseInt(device.getKey());
+
+		if (theCommand.equals("volume"))
+		{
+			returnVal = integraHelper.outVolume(key, extra);
+		}
+		else if (theCommand.equals("mute"))
+		{
+			returnVal = integraHelper.outMute(key, extra);
+		}
+		else if (theCommand.equals("power")) // may need to add on and off??
+		{
+			returnVal = integraHelper.outPower(key, extra);
+		}
+		else if (theCommand.equals("src"))
+		{
+			try 
+			{
+				extra = getCatalogueValue(extra, "AV_INPUTS", device);
+				if (extra != null)
+					returnVal = integraHelper.outSource(key, extra);
+			} 
+			catch (ParameterException e) {
+				e.printStackTrace();
+			}
+		}
+		// FIXME change the flash command? we have a preset as well...
+		else if (theCommand.equals("preset"))
+		{
+			try
+			{
+				extra = getCatalogueValue(extra, "AV_SURROUND", device);
+				if (extra != null)
+					returnVal = integraHelper.outPreset(key, extra);
+			}
+			catch (ParameterException e) {
+				e.printStackTrace();
+			}
+		}
+		else
+		{
+			logger.log (Level.WARNING,"Integra does not handle command ["+ theCommand +"] from client yet");			
+		}
+		return returnVal;
+	}
+}
+
+
+	
