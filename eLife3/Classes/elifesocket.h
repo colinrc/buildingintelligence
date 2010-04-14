52f911b5e58c06ee21ffd2d6e003ba544eda6341
cameron <cameron@bi>
2010-04-14 06:43:47 +0000

    latest version

---
+++ eLife3/Classes/elifesocket.h
@@ -0,0 +1,26 @@
+//
+//  socket.h
+//  eLife3
+//
+//  Created by Cameron Humphries on 16/01/10.
+//  Copyright 2010 Humphries Consulting Pty Ltd. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+
+@interface elifesocket : NSObject {
+	NSInputStream *iStream;
+	NSOutputStream *oStream;
+	NSInteger error_status;
+	
+}
+@property (nonatomic, retain) NSInputStream *iStream;
+@property (nonatomic, retain) NSOutputStream *oStream;
+@property (nonatomic) NSInteger error_status;
+
+- (void)connectToELife;
+- (void)sendMessage;
+- (void)alertOtherAction;
+
+@end
