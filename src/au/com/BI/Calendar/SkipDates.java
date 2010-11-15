6ae94c90532ef2a81df93f1a336c1c21b564697f
richard <richard@bi>
2010-11-15 05:52:29 +0000

    fixed warnings by adding types to some containers

--- src/au/com/BI/Calendar/SkipDates.java
+++ src/au/com/BI/Calendar/SkipDates.java
@@ -12,19 +12,19 @@ public class SkipDates {
 	
     protected SimpleDateFormat skipDate = null;
     protected Logger logger;
-    protected List skipDates = null;
+    protected List<SkipEntry> skipDates = null;
 
     public SkipDates () {
 		logger = Logger.getLogger(this.getClass().getPackage().getName());
 
         skipDate = (SimpleDateFormat)SimpleDateFormat.getDateTimeInstance();
         skipDate.applyPattern("yyyy-MM-dd hh:mm:ss");
-        skipDates = new LinkedList ();    	
+        skipDates = new LinkedList<SkipEntry>();    	
     }
     
 	public void parseEvent (Element event){
-        List skipElements = event.getChildren("skip");
-        Iterator eachSkip = skipElements.iterator();
+        List<Element> skipElements = event.getChildren("skip");
+        Iterator<Element> eachSkip = skipElements.iterator();
         while (eachSkip.hasNext()){
         		Element skipElement = (Element)eachSkip.next();
         		String skipStart = skipElement.getAttributeValue("start_date") + " 00:00:00";
@@ -34,9 +34,9 @@ public class SkipDates {
 	}
 	
 	public void renderEvent (Element event){
-		Iterator eachSkip = skipDates.iterator();
+		Iterator<SkipEntry> eachSkip = skipDates.iterator();
 		while (eachSkip.hasNext()){
-			SkipEntry skipEntry = (SkipEntry)eachSkip.next();
+			SkipEntry skipEntry = eachSkip.next();
 			String startDate = skipDate.format(skipEntry.startDate);
 			String endDate = skipDate.format(skipEntry.endDate);					
 			Element skipXML = new Element("skip");
@@ -58,13 +58,12 @@ public class SkipDates {
 	}
 	
 	public boolean doISkipDate(Date testDate){
-		Iterator eachSkip = skipDates.iterator();
+		Iterator<SkipEntry> eachSkip = skipDates.iterator();
 		while (eachSkip.hasNext()){
-			SkipEntry skipEntry = (SkipEntry)eachSkip.next();
+			SkipEntry skipEntry = eachSkip.next();
 			if ((skipEntry.getStartDate().compareTo(testDate) <= 0 ) &&
 					(skipEntry.getEndDate().compareTo(testDate) >= 0 ))
 				return true;
-			
 		}
 		return false;
 	}
