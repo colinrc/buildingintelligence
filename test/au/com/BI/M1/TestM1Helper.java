102cd5476811405a0ce4128670e9db9397e7840d
richard <richard@bi>
2010-11-17 22:19:52 +0000

    fixed test and finished the uncompleted tests

--- test/au/com/BI/M1/TestM1Helper.java
+++ test/au/com/BI/M1/TestM1Helper.java
@@ -10,14 +10,12 @@ public class TestM1Helper {
 	@Before
 	public void setUp () {
 		m1Helper = new M1Helper();
-		
 	}
 
 	@Test
 	public void testCalcM1Checksum() {
 		assertEquals("Generate checksum failed","66",m1Helper.calcM1Checksum("06as00"));
-		// FIXME there is an issue below 0e != 0E, which is correct?
-		assertEquals("Generate checksum failed","0e",m1Helper.calcM1Checksum("1EAS10000000400000003000000000"));		
+		assertEquals("Generate checksum failed","0E",m1Helper.calcM1Checksum("1EAS10000000400000003000000000"));		
 	}
 
 	@Test
@@ -25,12 +23,11 @@ public class TestM1Helper {
 		assertEquals("Pass checksum failed",true,m1Helper.passChecksum("06as0066"));
 		assertEquals("Pass checksum failed",true,m1Helper.passChecksum("1EAS100000004000000030000000000E"));
 		assertEquals("Pass checksum failed",false,m1Helper.passChecksum("06as0065"));		
-
 	}
 
 	@Test
 	public void testBuildCompleteM1String() {
-
+		assertEquals("Pass build string failed","1EAS100000004000000030000000000E",m1Helper.buildCompleteM1String("AS10000000400000003000000000"));
+		assertEquals("Pass build string failed","06as0066",m1Helper.buildCompleteM1String("as00"));
 	}
-
 }
